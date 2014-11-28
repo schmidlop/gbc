bd355b1c0000c14f62cebd9c6cc82d23f1908fd1
Brian Schmitt <brian.schmitt@pbb.me>
2014-11-28 11:50:41 -0500

    progress

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Start sails and pass it command line arguments
-require('sails').lift(require('optimist').argv);
+//require('sails').lift(require('optimist').argv);
 
-
+require('sails').lift({port:3000, host:'127.0.0.1', environment:'production'});
 //$(".owl-carousel").owlCarousel({items:1,dots:false,loop:true,nav:true});

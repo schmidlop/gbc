16a27d4d4664c02d9ebc0bc50c5601a6ac46c3df
Brian Schmitt <brian.schmitt@pbb.me>
2014-11-30 10:49:06 -0500

    change host setting

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Start sails and pass it command line arguments
 //require('sails').lift(require('optimist').argv);
 
-require('sails').lift({port:80, host:'172.31.43.37', environment:'production'});
+require('sails').lift({port:80, host:'gracebaptist.church', environment:'production'});
 //$(".owl-carousel").owlCarousel({items:1,dots:false,loop:true,nav:true});
